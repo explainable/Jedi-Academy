179bd920e05848f8fdbd07ea743b53f27bb6b9bd
Sebastian Coates <sebass@Sebastians-MacBook-Pro-2.local>
2019-10-17 14:36:40 -0400

    Perturbation generator working and tested for 2d (grayscale) images

--- image_processing/perturbation_generator.py
+++ image_processing/perturbation_generator.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class GrayscalePerturbator:
-    def __init__(self, image):
+    def __init__(self, image, sample_image=True, grid_dimen=2, stride=2, scale_factor=2):
         try: 
-            self.original = np.array(image, copy=True)
+            self.original = np.array(image, copy=True, dtype="float32")
         except:
             raise Exception("Unable to convert input to NumPy array")
 
@@ -12,25 +12,74 @@ class GrayscalePerturbator:
             if len(shape) == 1:
                 raise Exception("Expecting 2d array of pixels, but got 1d array")
 
-            if len(shape) == 3 and shape[2] != 1:
+            if len(shape) == 3:
                 raise Exception("Expecting 2d array of pixels, but got 3d array")
 
             raise Exception("Expecting 2d array of pixels, but got " + str(len(shape)) + "d array")
+        self.height, self.width = shape
 
+        if (sample_image):
+            flat = self.original.flatten() #since mean and var both flatten
+            self.mean = np.mean(flat)
+            self.variance = np.var(flat)
+        else:
+            self.mean = 122.5
+            self.variance = 30
 
-        self.currentIteration = 0
+        self.maskPosition = (0, 0)
         self.done = False
+        self.grid_dimen = grid_dimen
+        self.stride = stride
+        self.scale_factor = scale_factor
 
+    # returns a copy to avoid clients breaking internal logic
+    # could return a reference while storing memory of pre-perturbed grid,
+    #       but likely not much of a performance improvement, esp for small img
     def nextPerturbation(self):
         if self.done:
             return None
 
-        perturbation = np.copy(self.original)
+        perturbation = self.applyPerturbation(np.copy(self.original))
 
-        # apply perturbation
-
-        self.currentIteration += 1
-
-        # check if done, update self.done accordingly
+        self.updateMask()
 
         return perturbation
+
+    def __iter__(self):
+        return self;
+
+    def __next__(self):
+        pert = self.nextPerturbation()
+        if (pert is None):
+            raise StopIteration()
+        return pert
+
+    def applyPerturbation(self, perturbation):
+        mask = np.random.normal(loc=self.mean, scale=self.variance, size=(self.grid_dimen, self.grid_dimen))
+
+        posX, posY = self.maskPosition
+        for r, row in enumerate(mask):
+            for c, col in enumerate(mask):
+                perturbation[posY + r][posX + c] = mask[r][c] 
+
+        return perturbation
+
+    def updateMask(self):
+        posX, posY = self.maskPosition
+        dimen = self.grid_dimen
+        stride = self.stride
+
+        xOver = posX + dimen + stride > self.width
+        yOver = posY + dimen + stride > self.height
+
+        if xOver and yOver:
+            self.maskPosition = (0, 0)
+            self.grid_dimen *= self.scale_factor
+            self.stride *= self.scale_factor
+
+            if (self.grid_dimen > self.width / 2 or self.grid_dimen > self.height / 2):
+                self.done = True
+        elif xOver:
+            self.maskPosition = (0, posY + stride)
+        else:
+            self.maskPosition = (posX + stride, posY)
--- image_processing/run_tests.py
+++ image_processing/run_tests.py
@@ -9,9 +9,12 @@ except Exception as e:
     else:
         print("1d array test passed")
 
-
-generator2d = GrayscalePerturbator([[1,2,3,4,5], [1, 2, 3, 4, 5]])
-print("2d array test passed")
+try: 
+    generator2d = GrayscalePerturbator([[1,2,3,4,5], [1, 2, 3, 4, 5]])
+    print("2d array test passed")
+except Exception as e: 
+    print("Valid input threw exception for 2d array initializer: ")
+    print(str(e))
 
 try:
     generator2d = GrayscalePerturbator([[[1,2,3,4,5], [1, 2, 3, 4, 5]], [[1,2,3,4,5], [1, 2, 3, 4, 5]]])
@@ -30,3 +33,62 @@ except Exception as e:
     else:
         print("4d array test passed")
 # End 2D Array Initializer Tests #
+
+
+
+# Begin 2D Array Perturbation Tests #
+try:
+    generator2d = GrayscalePerturbator([[1, 0, 1, 0], 
+                                        [0, 1, 0, 1],
+                                        [1, 0, 1, 0],
+                                        [0, 1, 0, 1]])
+
+    perturbed = generator2d.nextPerturbation()
+    while(perturbed is not None):
+        perturbed = generator2d.nextPerturbation()
+    print("Default perturbation passed")
+except Exception as e: 
+    print("Valid input threw exception for default perturbation: ")
+    print(str(e))
+
+try:
+    generator2d = GrayscalePerturbator([[1, 0, 1, 0], 
+                                        [0, 1, 0, 1],
+                                        [1, 0, 1, 0],
+                                        [0, 1, 0, 1]], grid_dimen=1, stride=1)
+
+    perturbed = generator2d.nextPerturbation()
+    while(perturbed is not None):
+        perturbed = generator2d.nextPerturbation()
+    print("Scaling perturbation passed")
+except Exception as e: 
+    print("Valid input threw exception for scaling perturbation: ")
+    print(str(e))
+
+try:
+    generator2d = GrayscalePerturbator([[1, 0, 1, 0], 
+                                        [0, 1, 0, 1],
+                                        [1, 0, 1, 0],
+                                        [1, 0, 1, 0],
+                                        [1, 0, 1, 0]], grid_dimen=1, stride=1)
+
+    perturbed = generator2d.nextPerturbation()
+    while(perturbed is not None):
+        perturbed = generator2d.nextPerturbation()
+    print("Non-square perturbation passed")
+except Exception as e: 
+    print("Valid input threw exception for Non-square perturbation: ")
+    print(str(e))
+
+try:
+    for pert in GrayscalePerturbator([[1, 0, 1, 0], 
+                                            [0, 1, 0, 1],
+                                            [1, 0, 1, 0],
+                                            [1, 0, 1, 0],
+                                            [1, 0, 1, 0]], grid_dimen=1, stride=1):
+        pass
+    print("Perturbation generator syntax passed")
+except Exception as e: 
+    print("Valid input threw exception for generator syntax: ")
+    print(str(e))
+# End 2D Array Perturbation Tests #
