f63fe211d75495fbfb65a6fe5b445ea5ee3c9e12
John Tagliaferro <j22tag@gmail.com>
2019-10-26 17:18:25 -0400

    install image_processing package locally working

---
+++ image_processing/__init__.py
@@ -0,0 +1,1 @@
+from .perturbation_generator import GrayscalePerturbator
---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+DESC = 'This is a module to do ML stuff' # TODO: Seb/Aidan make this not bad
+
+setup(name='Jedi-Academy-Explainable',
+      version='0.1',
+      description=DESC,
+      url='http://github.com/explainable/Jedi-Academy',
+      author='Explainable',
+      author_email='explainable@outlook.com',
+      #liscense='MIT', # TODO: Fill this is when we decide on what Liscense
+      packages=['image_processing'],
+      zip_safe=False # Not sure if this should be true or false, false seemed safer
+)
