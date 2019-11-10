2c8b64b76925f0882d9f5be8e3562691e4971111
Sebastian Coates <sebass@Sebastians-MacBook-Pro-2.local>
2019-11-09 20:26:05 -0500

    Script for generating all heatmaps

---
+++ image_processing/generate_heatmaps.sh
@@ -0,0 +1,4 @@
+for i in {0..9}
+do
+python3 mnist_perturbation_analyzer.py {$i}_out $i
+done
--- image_processing/mnist_perturbation_analyzer.py
+++ image_processing/mnist_perturbation_analyzer.py
@@ -6,16 +6,34 @@ from PIL import Image
 
 from sys import argv 
 
+sample = 0
 if len(argv) == 1:
     print("No filename provided, using default")
     filename = ""
+    
 else:
     filename = argv[1] + "_"
+    if len(argv) > 2:
+        sample = int(argv[2])
 
-ADVERSARIAL = True
+ADVERSARIAL = False
+
+NUMBER_TO_SAMPLE_INDEX = {
+    0: 1,
+    1: 6,
+    2: 5,
+    3: 7,
+    4: 2,
+    5: 0,
+    6: 13,
+    7: 15,
+    8: 17,
+    9: 4
+}
+sampleIndex = NUMBER_TO_SAMPLE_INDEX[sample]
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-sample_image = (x_train[7]) # by 17, all 10 digits are represented at least once
+sample_image = (x_train[sampleIndex]) # by 17, all 10 digits are represented at least once
 generator2d = GrayscalePerturbator(sample_image, grid_dimen=1, stride=1, stride_scale=1, cutoff_dimen=28, is_adversarial=ADVERSARIAL)
 model = get_pretrained_mnist_cnn()
 
@@ -24,8 +42,7 @@ if K.image_data_format() == 'channels_first':
 else:
     input_shape = (28, 28, 1)
 
-
-target = np.argmax(model.predict(sample_image.reshape(1, 28, 28, 1)))
+target = y_train[sampleIndex]
 
 misses = np.zeros((28, 28), dtype="float32")
 for pert in generator2d:
